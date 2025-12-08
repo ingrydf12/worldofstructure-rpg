eafee27dbb27d1691c58eae59b80cdd4a757c304
Ingryd <ingrydcduarte@gmail.com>
2025-12-08 17:56:46 -0300

    classe de herança: skeleton

--- motor/external_atributtes.py
+++ motor/external_atributtes.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
-class_torm_types = {'warrior':'Guerreiro', 'archer':'Atirador', 'magician':'Mago'};
-
-@abstractmethod
 class Skeleton(ABC):
-    def __init__(self, life, atributtes):
-        self.life = None;
+    def __init__(self, name, life, attributes):
+        self.name = name
+        self.life = life
+        self.attributes = attributes
 
-    def constructor_persona(class_type):
-        class_type = class_torm_types.warrior
+    @abstractmethod
+    def attack(self, target):
+        pass
